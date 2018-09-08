451ce3f1a521e350e930d7dc4bfbf4b426136d3b
maunil298 <41335066+maunil298@users.noreply.github.com>
2018-09-09 01:34:26 +0530

    few changes
    few changes

--- main.py
+++ main.py
@@ -2,51 +2,69 @@
 import requests
 import os
 import re
-# from pprint import pprint
-# clear = lambda: os.system('cls')
+
+#get all folder names in a list
 folderlist = os.listdir()
+
+#function for removing brackets or any other symbol specified
 def mname(name,symb):
 	x = name.rfind(symb)
 	newname = name[0:x]
 	return newname
+
+#this function searches for title and gets json from omdb api, and returns string as imdbrating-title-year from json
 def searchomdb(title):
-	req = requests.get("http://www.omdbapi.com/?apikey=" + omdbkey +"&t=" + title)
-	maunil = (req.json())
 	try:
 		pass
-		imrating = (maunil['imdbRating'] + " - " + title + " " + "(" +maunil['Year'] + ")")
-		return imrating
+		#search for title
+		req = requests.get("http://www.omdbapi.com/?apikey=" + omdbkey +"&t=" + title)
+		
+		# store requested json to dictionary
+		req_dictionary = (req.json())
+		
+		name_with_rating = (req_dictionary['imdbRating'] + " - " + title + " " + "(" +req_dictionary['Year'] + ")")
+		return name_with_rating
 	except:
-		return oldname
+		return False
 
-def rename(path_from,path_to):
-	absolute_from = os.path.join(os.getcwd(), path_from)
-	absolute_to = os.path.join(os.getcwd(), path_to)
+def rename(old_foldername,new_foldername):
+	old_path = os.path.join(os.getcwd(), old_foldername)
+	new_path = os.path.join(os.getcwd(), new_foldername)
 	try:
-		os.rename(absolute_from, absolute_to)
+		os.rename(old_path, new_path)
 		return True
 	except:
 		return False
+
+
 omdbkey = input ("Enter omdb api key : ")
-	
-for i in folderlist:
-	oldname = i
-	c = i.count('[')
-	d = i.count('(')
+
+print(" ")
+print("Old Folder Name".ljust(25,' ') + "Status".ljust(25,' ') + "Renamed to")
+print(" ")
+for folder in folderlist:
+	oldname = folder
+	symbol_one = folder.count('[')
+	symbol_two = folder.count('(')
 	a = 0
-	while a<c:
+	while a<symbol_one:
 		pass
-		i = mname(i,"[")
+		folder = mname(folder,"[")
 		a = a+1
 	a = 0
-	while a<d:
+	while a<symbol_two:
 		pass
-		i = mname(i,"(")
+		folder = mname(folder,"(")
 		a = a+1
 
-	aaa = searchomdb(i)
-	print (aaa)
-	rename(oldname,aaa)
+	new_foldertitle = searchomdb(folder)
+	
+	if new_foldertitle == False:
+		print (oldname.ljust(25,' ') + "Not Found ")
+	else:
+		print (oldname.ljust(25,' ') + "Renamed  ".ljust(25,' ') + new_foldertitle)
+		rename(oldname,new_foldertitle)
 
 print(' ')
 os.system('pause')
+
